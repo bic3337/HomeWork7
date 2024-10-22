e3d1bbe1b10168c5d5e84d84d03817a514443f95
bic33 <bic3337@gmail.com>
2024-10-22 22:55:18 +0300

    Для д\з 1.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 7.");
-
+        System.out.println("Задача 1.");
+        int[] book = new int[]{1, 2, 3};
+        float[] weigths = new float[]{1.57f, 7.654f, 9.986f};
+        long[] ants = new long[]{250_000_000L, 6_000_000_000L,};
+        System.out.println("Задача 2 .");
+        System.out.print(book[0]+",");
+        System.out.print(book[1]+"," );
+        System.out.print(book[2]+"");
+        System.out.println(" ");
+        System.out.print(weigths[0]+",");
+        System.out.print(weigths[1]+",");
+        System.out.print(weigths[2]+"");
+        System.out.println(" ");
+        System.out.print(ants[0]+",");
+        System.out.print(ants[1]+"");
     }
 }
+
